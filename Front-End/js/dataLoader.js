58b9b0c25fb9950ebb743ea888ed437308946a7f
Nime <sebastianz541@googlemail.com>
2017-06-06 21:48:21 +0200

    separated files and made diagramm relative

--- Front-End/js/dataLoader.js
+++ Front-End/js/dataLoader.js
@@ -1,7 +1,13 @@
-var DataLoader = function(){
-	this.loadExecutions(){
-		console.log("Hi");
-		return jQuery.getJSON("json/executions.json");
-	}
-	return this;
-}
+var DataLoader = window.DataLoader || {};
+var DataLoader = function(defaultFunction){
+    var callbackFunction = defaultFunction;
+    this.setCallbackFunction = function(callback){
+        callbackFunction = callback;
+    };
+    this.loadExecutions = function(){
+        d3.json("json/executions.json", function(json){
+            callbackFunction(json);
+        });
+    };
+    return this;
+};
--- Front-End/js/timeline.js
+++ Front-End/js/timeline.js
@@ -1,43 +1,57 @@
-var Timeline = function(executions){
-    var range = [1400, 1800];
-    var executions = [1405, 1506, 1788, 1533, 1777, 1567, 1456, 1576, 1456, 1435, 1533, 1533, 1533, 1456, 1456, 1777, 1777, 1777, 1630, 1630, 1630, 1630, 1630, 1630, 1630, 1630, 1630];
+var Timeline = function(svg, executions, range){
+    var timeline = d3.select(svg);
     var execMap = [];
     var maxExPYe = 0; //Maximum of executions per Year
-    var number = 0;
-    execMap.addData = function(data){
+    var width = 400,
+        height = 200;
+    init();
+
+    function init(){
+        createYearMap(executions);
+        drawData();
+        drawLine();
+    }
+
+    function createYearMap(data){
         var index = 0;
         var yearMap = [];
-        for(x in data){
+        for(var x in data){
             index = data[x];
-            if(yearMap[index] == undefined){
+            if(yearMap[index] === undefined){
                yearMap[index] = 0;
             }
             yearMap[index]++;
         }
-        for(x in yearMap){
+        for(var x in yearMap){
             execMap.push([x, yearMap[x]]);
             if(maxExPYe < (yearMap[x]*1)){
-                maxExPYe = yearMap[x]*1
+                maxExPYe = yearMap[x]*1;
             }
         }
+    };
+
+    function drawData(){
+        var scaleHeight = d3.scaleLinear().domain([0, maxExPYe]).range([0, 100]);
+        var scaleXpos = d3.scaleLinear().domain([range.start*1, range.end*1]).range([0, 100]);
+        timeline.attr("width", width)
+            .attr("height", height);
+        var bar = timeline.selectAll("g")
+            .data(execMap)
+            .enter().append("rect")
+                .attr("x", function(d){return -scaleXpos(d[0]) + "%"})
+                .attr("y", "-100%")
+                .attr("transform", "rotate(180)")
+                .attr("width", 1)
+                .attr("height", function(d){return scaleHeight(d[1]) + "%";});
+    };
+
+    function drawLine(){
+        timeline.append("line")
+            .attr("x1", "0%")
+            .attr("x2", "100%")
+            .attr("y1", "100%")
+            .attr("y2", "100%")
+            .attr("style", "stroke:rgb(0, 0, 0); stroke-width:3px; z-index:1");
     }
-    execMap.addData(executions);
-    var width = 400,
-        height = 200;
-    console.log("max: " + maxExPYe);
-    var scaleHeight = d3.scaleLinear().domain([0, maxExPYe]).range([0, height]);
-    var scaleXpos = d3.scaleLinear().domain([range[0], range[1]]).range([0, width]);
-    var timeline = d3.select("svg")
-        .attr("width", width)
-        .attr("height", height);
-    var bar = timeline.selectAll("g")
-        .data(execMap)
-        .enter().append("g")
-             .attr("transform", function(d, i) {console.log(d[0]*1 + " Xpos: " + scaleXpos(d[0]*1)); return "translate(" + scaleXpos(d[0]*1) + ", 200) rotate(180)"});
-    console.log("number: " + number);
-    bar.append("rect")
-        .attr("width", 1)
-        .attr("height", function(d){return scaleHeight(d[1])});
-		
-	return this;
+    return this;
 };
