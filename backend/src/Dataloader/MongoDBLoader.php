04bdc59b8df15eddc4e94909d36e0de90797c59e
Iranox <aasgar@arcor.de>
2017-06-07 11:59:31 +0200

    refactor

--- backend/src/Dataloader/MongoDBLoader.php
+++ backend/src/Dataloader/MongoDBLoader.php
@@ -1,30 +1,33 @@
 <?php
 require '../vendor/autoload.php'; // include Composer's autoloader
 
-class CSVParser {
-  private function loadCSV($file){
-    $csv = array_map('str_getcsv', file($file));
-    array_walk($csv, function(&$a) use ($csv) {
-      $a = array_combine($csv[0], $a);
-   });
-   array_shift($csv); # remove column header
-   return $csv;
-  }
+class CSVParser{
+    private function loadCSV($file)
+    {
+        $csv = array_map('str_getcsv', file($file));
+        array_walk($csv, function (&$a) use ($csv) {
+            $a = array_combine($csv[0], $a);
+        });
+        array_shift($csv); # remove column header
+        return $csv;
+    }
 
-  public function parse($file){
-    return $this->loadCSV($file);
-  }
+    public function parse($file)
+    {
+        return $this->loadCSV($file);
+    }
 }
 
 class MongoLoader{
-  public function insertDocuments($data){
-    $client = new MongoDB\Client("mongodb://localhost:27017");
-    $collection = $client->hexenverfolgung->verfolgung;
+    public function insertDocuments($data)
+    {
+        $client = new MongoDB\Client("mongodb://localhost:27017");
+        $collection = $client->hexenverfolgung->verfolgung;
 
-    for($i = 0; $i < count($data);$i++){
-      $result = $collection->insertMany([$data[$i]]);
+        for ($i = 0; $i < count($data); $i++) {
+             $collection->insertMany([$data[$i]]);
+        }
     }
-  }
 }
 
 
