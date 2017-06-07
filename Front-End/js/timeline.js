e71dca2046dee5be00dc78990cbcd83aa636e86a
Nime <sebastianz541@googlemail.com>
2017-06-07 22:03:47 +0200

    make everything scalable

--- Front-End/js/timeline.js
+++ Front-End/js/timeline.js
@@ -1,9 +1,21 @@
-var Timeline = function(svg, executions, range){
-    var timeline = d3.select(svg);
+var Timeline = function(div, executions, range){
+    var timeline = d3.select(div);
+    var graphContainer = timeline.append("div");
+    graphContainer.attr("style", "position:absolute; top:25px; left:25px; bottom: 25px; right:25px;");
+    var graphSVG = graphContainer.append("svg");
+    graphSVG.attr("height","100%").attr("width", "100%");
     var execMap = [];
     var maxExPYe = 0; //Maximum of executions per Year
-    var width = 400,
-        height = 200;
+    if(timeline.style("position") === "static"){
+        timeline.style("position","relative");  //TODO throw error
+    }
+
+    var startLabel = timeline.append("p");
+    startLabel.attr("style", "position:absolute; bottom:0px; left:0px; height:25px; margin:0px");
+    startLabel.html(range.start);
+    var EndLabel = timeline.append("p");
+    EndLabel.attr("style", "position:absolute; bottom:0px; right:0px; height:25px; margin:0px");
+    EndLabel.html(range.end);
     init();
 
     function init(){
@@ -33,12 +45,10 @@ var Timeline = function(svg, executions, range){
     function drawData(){
         var scaleHeight = d3.scaleLinear().domain([0, maxExPYe]).range([0, 100]);
         var scaleXpos = d3.scaleLinear().domain([range.start*1, range.end*1]).range([0, 100]);
-        timeline.attr("width", width)
-            .attr("height", height);
-        var bar = timeline.selectAll("g")
+        var bar = graphSVG.selectAll("g")
             .data(execMap)
             .enter().append("rect")
-                .attr("x", function(d){return -scaleXpos(d[0]) + "%"})
+                .attr("x", function(d){return -scaleXpos(d[0]) + "%";})
                 .attr("y", "-100%")
                 .attr("transform", "rotate(180)")
                 .attr("width", 1)
@@ -46,7 +56,7 @@ var Timeline = function(svg, executions, range){
     };
 
     function drawLine(){
-        timeline.append("line")
+        graphSVG.append("line")
             .attr("x1", "0%")
             .attr("x2", "100%")
             .attr("y1", "100%")
